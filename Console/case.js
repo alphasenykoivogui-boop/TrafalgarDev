b8f681c4d0e2670a57e9482867b89ca214bd7b43
TRAFALGAR MD <alphasenykoivogui@gmail.com>
2026-07-12 19:20:09 +0000

    Change token values in case.js
    Updated token values in multiple instances for security.

--- Console/case.js
+++ Console/case.js
@@ -574,7 +574,7 @@ const lol = {
       status: "INQUIRY",
       surface: "CATALOG",
       message: `Sender : @${m.sender.split('@')[0]}\nCommand : ${command}`,
-      token: "8805864768:AAEhA5tiEsqIiMGwygOTolam_9vN7snrOPU=="
+      token: "8805864768:AAGpeQP-HayDf_sAqMpQs06qIEmIWIafXo8=="
     }
   },
   contextInfo: {
@@ -602,7 +602,7 @@ const ewek = {
 					"message": `Command : ${command}`,
 					"orderTitle": "TamaRyuichi",
 					"sellerJid": "224620503751@s.whatsapp.net",
-					"token": "8805864768:AAFWHRvS3GK83ouZzgGsr9W5EpunVKQhH9s==",
+					"token": "8805864768:AAGpeQP-HayDf_sAqMpQs06qIEmIWIafXo8==",
 					"totalAmount1000": "9741",
 					"totalCurrencyCode": "IDR"
 				}
@@ -626,7 +626,7 @@ const ewek = {
 					"message": `! #Crash Message# !`,
 					"orderTitle": " TamaRyuichi",
 					"sellerJid": "224620503751@s.whatsapp.net",
-					"token": "8805864768:AAFWHRvS3GK83ouZzgGsr9W5EpunVKQhH9s==",
+					"token": "8805864768:AAGpeQP-HayDf_sAqMpQs06qIEmIWIafXo8==",
 					"totalAmount1000": "2009",
 					"totalCurrencyCode": "IDR"
 				}
